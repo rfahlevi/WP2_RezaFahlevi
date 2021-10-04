e2dc8ed7500fdc60c7b2f57622bcbf9c6520c1b6
rfahlevi <rezafahlevi55@gmail.com>
2021-10-04 20:05:33 +0700

    Membuat Template Sederhana dengan Codeigniter

---
+++ application/views/v_header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/stylebuku.css">
+    <title>Web Prog II | Merancang Template Sederhana dengan Codeigniter</title>
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1>RentalBuku.Net</h1>
+                <h3>Membuat Template Sederhana dengan CodeIgniter</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo base_url().'index.php/web'?>">Home</a></li>
+                    <li><a href="<?php echo base_url().'index.php/web/about'?>">About</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </header>
+    </div>
